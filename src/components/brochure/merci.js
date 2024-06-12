6f81ecf05ece769bd5c13fa7c8d811cda9ed8a4e
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2024-06-12 17:42:10 +0100

    ff

--- src/components/brochure/merci.js
+++ src/components/brochure/merci.js
@@ -17,7 +17,7 @@ const Merci = () => {
         top: done !== null ? "14%" : "100%",
         opacity: done !== null ? 1 : 0,
       }}
-      className="z-50 w-full pb-32 md:py-28 bg-[#F4F4F4] "
+      className="z-50 w-full pb-48 md:py-28 bg-[#F4F4F4] "
     >
       <div className="flex justify-center items-center pl-5 md:pl-0">
         <h1 className="semi text-xl md:text-2xl">
